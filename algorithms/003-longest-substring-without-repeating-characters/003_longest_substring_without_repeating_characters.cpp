067118c5366a0e1b55ee84b5e1238d28e760756e
Rowan-James Tran <rowanjames.tran@gmail.com>
2019-08-16 23:20:37 -0700

    Longest Substring solution

--- algorithms/003-longest-substring-without-repeating-characters/003_longest_substring_without_repeating_characters.cpp
+++ algorithms/003-longest-substring-without-repeating-characters/003_longest_substring_without_repeating_characters.cpp
@@ -3,33 +3,25 @@
  */
 
 #include <algorithm>
+#include <unordered_set>
 
-using std::count;
+using std::unordered_set;
 
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         string::size_type max_length = 0;
         for (string::size_type i = 0; i < s.size(); i++) {
-            for (string::size_type j = i + max_length + 1; j <= s.size(); j++) {
-                string substring = s.substr(i, j-i);
-                if (!has_repeating_characters(substring)) {
-                    max_length = substring.size();
-                }
+            unordered_set<char> chars;
+            string::size_type j = i;
+            while (j < s.size() && !chars.count(s[j])) {
+                chars.insert(s[j]);
+                ++j;
             }
+
+            max_length = std::max(max_length, j - i);
         }
 
         return max_length;
-    }
-
-private:
-    bool has_repeating_characters(string s) {
-        for (char c : s) {
-            if (count(s.begin(), s.end(), c) > 1) {
-                return true;
-            }
-        }
-
-        return false;
     }
 };
