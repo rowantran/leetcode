f64877754f6b920d8e6c07304fb7582b265443d8
Rowan-James Tran <rowanjames.tran@gmail.com>
2020-06-03 02:06:44 -0700

    Update comments for Single Element

--- algorithms/540-single-element-in-a-sorted-array/540_single_element_in_a_sorted_array.cpp
+++ algorithms/540-single-element-in-a-sorted-array/540_single_element_in_a_sorted_array.cpp
@@ -8,18 +8,8 @@ class Solution {
 public:
     typedef vector<int>::size_type size_v;
 
-    // First, we must consider that with n pairs of integers and 1 lone integer, the total size
-    // of the initial array will always be 2n+1 (odd). So calculating (low+high)/2 for the initial
-    // array is always a "true" middle with the same number of elements before and after it.
-
-    // We want to find either the exact middle element if [0, mid] is an even number of elements,
-    // or middle-1 if [0, mid] is an odd number. This ensures that if there are *only* pairs
-    // before the middle element, the middle element will always be the second element of its pair.
-    // If the middle element is not the second element of its pair, we know that order has been
-    // disturbed *before* the middle element.
-    
-    // Furthermore, this method of calculating the middle element ensures that [low, mid-1]
-    // and [mid+1, high] always have an odd number of elements as well.
+    // This function calculates an appropriate "middle" value to use when searching the array.
+    // The logic behind subtracting 1 is explained in singleNonDuplicateHelper.
     size_v find_mid(size_v low, size_v high) {
         size_v mid = (low + high) / 2;
         if ((mid % 2) != 0) {
@@ -32,13 +22,23 @@ public:
     // This function recursively searches the array from [low, high] for the lone integer.
     // The logic behind the search is explained in the function.
     int singleNonDuplicateHelper(const vector<int>& nums, size_v low, size_v high) {
+        // This base condition works because [low, high] has an odd number of elements
+        // at all times.
         if (low == high) return nums[low];
 
         size_v mid = find_mid(low, high);
 
-        // If the middle index of the initial array is the first member of its pair,
-        // the lone integer must be at an index to the left. Conversely, if the middle index is the
-        // second member of its pair, the lone integer must be at an index to the right.
+        // We want to find either the exact middle element if [0, mid] is an even number of elements,
+        // or middle-1 if [0, mid] is an odd number. This ensures that [0, n] where n is either middle
+        // or middle-1 has an even number of elements. Then, if there are *only* pairs
+        // before the middle element, the middle element will always be the second element of its pair.
+        // If the middle element is not the second element of its pair, we know that order has been
+        // disturbed *before* the middle element.
+
+        // If the middle element is the first member of its pair, the lone integer must be at an index
+        // to the left. Conversely, if the middle element is the second member of its pair, the lone
+        // integer must be at an index to the right. And if the middle element is not a member of a
+        // pair, then we have found the desired element.
         if (nums[mid] == nums[mid+1]) {
             return singleNonDuplicateHelper(nums, low, mid-1);
         } else if (nums[mid] == nums[mid-1]) {
