bfc9949b1e6a0da2ac218bdaf03927c701264831
Rowan-James Tran <rowanjames.tran@gmail.com>
2020-06-06 02:34:14 -0700

    Initial solution for Print FooBar Alternately (too slow)

--- algorithms/1115-print-foobar-alternately/1115_print_foobar_alternately.cpp
+++ algorithms/1115-print-foobar-alternately/1115_print_foobar_alternately.cpp
@@ -5,33 +5,38 @@
 // The same instance of FooBar will be passed to two different threads. Thread A will call foo()
 // while thread B will call bar(). Modify the given program to output "foobar" n times.
 
-#include <mutex>
-
 class FooBar {
 private:
     int n;
-    mutex foobar_mutex;
+    int foosPrinted;
+    int barsPrinted;
 
 public:
-    FooBar(int n) {
+    FooBar(int n) : foosPrinted(0), barsPrinted(0) {
         this->n = n;
     }
 
     void foo(function<void()> printFoo) {
         for (int i = 0; i < n; i++) {
         	// printFoo() outputs "foo". Do not change or remove this line.
-            foobar_mutex.lock();
         	printFoo();
-            foobar_mutex.unlock();
+            ++foosPrinted;
+
+            while (foosPrinted > barsPrinted) {
+                // wait
+            }
         }
     }
 
     void bar(function<void()> printBar) {
         for (int i = 0; i < n; i++) {
-        	// printBar() outputs "bar". Do not change or remove this line.
-            foobar_mutex.lock();
-        	printBar();
-            foobar_mutex.unlock();
+            while (foosPrinted <= barsPrinted) {
+                // wait
+            }
+
+            // printBar() outputs "bar". Do not change or remove this line.
+            printBar();
+            ++barsPrinted;
         }
     }
 };
